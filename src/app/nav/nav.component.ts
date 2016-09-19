1a0a6e981c8393012a3bc68ffd4f854a5b1499b4
Dolan Miu <dolan_miu@hotmail.com>
2016-09-19 03:55:55 +0100

    added items to nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,27 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
-
+    public links = [
+        {
+            text: 'Home',
+            route: '/'
+        },
+        {
+            text: 'About Us',
+            route: 'about-us'
+        },
+        {
+            text: 'Committee',
+            route: 'committee'
+        },
+        {
+            text: 'Join Us',
+            route: 'join-us'
+        }
+    ];
 }
