5c256ef348376e53e249b42f4cb85ab4850bb167
Dolan Miu <dolan_miu@hotmail.com>
2016-10-03 23:32:29 +0100

    formatting and added committee members

--- src/app/committee/committee-members.ts
+++ src/app/committee/committee-members.ts
@@ -1,5 +1,6 @@
 interface Member {
     name: string;
+    position: string;
     description: string;
     photoUrl: string;
     facebook?: string;
@@ -13,11 +14,28 @@ export class Members extends Array<Member> {
         let members: Array<Member>;
         members = [
             {
+                name: 'Ioannis Karagiannakis',
+                position: 'President',
+                description: 'd',
+                facebook: '',
+                linkedIn: '',
+                photoUrl: ''
+            },
+            {
+                name: 'Christian Hirsekorn',
+                position: 'Treasurer',
+                description: 'd',
+                facebook: 'https://www.facebook.com/Partytempel?fref=ts',
+                linkedIn: 'https://www.linkedin.com/in/christian-hirsekorn-83398010a/en',
+                photoUrl: 'chris.jpg'
+            },
+            {
                 name: 'Pattawia Supradit',
+                position: 'Secretary',
                 description: 'Student at Queen Mary University of London with Excellence Scholarship',
                 facebook: 'https://www.facebook.com/pattawias',
                 linkedIn: 'https://www.linkedin.com/in/pattawia-supradit-249b98104',
-                photoUrl: './assets/pattawia.jpg'
+                photoUrl: 'pattawia.jpg'
             }
         ];
 
