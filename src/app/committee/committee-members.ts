2fe58805966f2a3334e06971998f48ddb5b0938d
Dolan Miu <dolan_miu@hotmail.com>
2016-10-16 14:38:49 +0100

    formatted committee section

--- src/app/committee/committee-members.ts
+++ src/app/committee/committee-members.ts
@@ -1,7 +1,6 @@
 interface Member {
     name: string;
     position: string;
-    description: string;
     photoUrl: string;
     facebook?: string;
     linkedIn?: string;
@@ -16,8 +15,6 @@ export class Members extends Array<Member> {
             {
                 name: 'Ioannis Karagiannakis',
                 position: 'President',
-                description: `Sexy Greek 20 year-old man, highly intelligent and sophisticated, 
-                    but slightly overweight, looking for classy and educated woman with similar ambitions`,
                 facebook: 'https://www.facebook.com/giannis.karagiannakis.9',
                 linkedIn: 'https://www.linkedin.com/in/ioannis-karagiannakis-50161188',
                 photoUrl: 'ioannis.jpg'
@@ -25,7 +22,6 @@ export class Members extends Array<Member> {
             {
                 name: 'Christian Hirsekorn',
                 position: 'Treasurer',
-                description: 'Sexy German',
                 facebook: 'https://www.facebook.com/Partytempel?fref=ts',
                 linkedIn: 'https://www.linkedin.com/in/christian-hirsekorn-83398010a/en',
                 photoUrl: 'chris.jpg'
@@ -33,9 +29,6 @@ export class Members extends Array<Member> {
             {
                 name: 'Pattawia Supradit',
                 position: 'Secretary',
-                description: `Economics and Finance student with industry experience in the Ministry of 
-                Finance (MoF), Royal Thai Embassy, Sales and Trading Summer analyst at BNP Paribas and Financial 
-                Institutions Coverage (FIC) at BNP Paribas`,
                 facebook: 'https://www.facebook.com/pattawias',
                 linkedIn: 'https://www.linkedin.com/in/pattawia-supradit-249b98104',
                 photoUrl: 'pattawia.jpg'
