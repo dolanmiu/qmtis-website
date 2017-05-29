baa0149bccf3502ac4ee497214868b254a601503
Dolan <dolan_miu@hotmail.com>
2017-05-29 22:33:45 +0100

    Added hero to reports

--- src/app/reports/reports.module.ts
+++ src/app/reports/reports.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
 import { PostCardComponent } from './post-card/post-card.component';
 import { ReportsComponent } from './reports.component';
 
@@ -7,6 +8,7 @@ import { ReportsComponent } from './reports.component';
     declarations: [
         ReportsComponent,
         PostCardComponent,
+        HeroComponent,
     ],
     imports: [
         CommonModule,
