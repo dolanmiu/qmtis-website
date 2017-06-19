b623800018b04dfa1c43c4d96ec018a1cfd84753
Dolan <dolan_miu@hotmail.com>
2017-06-19 03:30:00 +0100

    Initial work on pagination

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -2,18 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 import { BloggerService } from './blogger.service';
 
+interface PageReference {
+    token?: string;
+    id: number;
+}
+
 @Component({
     selector: 'app-reports',
     templateUrl: './reports.component.html',
     styleUrls: ['./reports.component.scss', '../shared/section.component.scss'],
 })
 export class ReportsComponent implements OnInit {
+    public pageReferences: PageReference[];
 
     constructor(private bloggerService: BloggerService) { }
 
     public ngOnInit(): void {
         this.bloggerService.blog().subscribe((res) => {
-            console.log(res);
+            this.pageReferences = Array(res.blog.posts.totalItems).fill({}).map((x, i) => {
+                return {
+                    id: i + 1,
+                };
+            });
+            console.log(this.pageReferences);
         });
     }
 }
