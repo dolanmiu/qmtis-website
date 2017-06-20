63485212a2298b95277858beeeb8601d7c16d184
Dolan <dolan_miu@hotmail.com>
2017-06-20 02:18:39 +0100

    Using dynamic blog for report page

--- src/app/reports/report/report.component.ts
+++ src/app/reports/report/report.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { BloggerService } from '../blogger.service';
 
 @Component({
     selector: 'app-report',
     templateUrl: './report.component.html',
     styleUrls: ['./report.component.scss', '../../shared/section.component.scss'],
 })
-export class ReportComponent {
+export class ReportComponent implements OnInit {
+    public post: Observable<Post>;
 
+    constructor(private activatedRoute: ActivatedRoute, private bloggerService: BloggerService) {
+    }
+
+    public ngOnInit(): void {
+        this.post = this.activatedRoute.queryParams.flatMap((params) => {
+            return this.bloggerService.post(params.id);
+        });
+    }
 }
