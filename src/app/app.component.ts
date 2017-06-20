4588d9f86092f1b9326e9aa6e42dda8f310a3cbd
Dolan <dolan_miu@hotmail.com>
2017-06-20 23:00:21 +0100

    Added window scrollTo when going to a next page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Location, PopStateEvent } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    private lastPoppedUrl: string;
 
+    constructor(private router: Router, private location: Location) {
+    }
+
+    public ngOnInit(): void {
+        this.location.subscribe((ev: PopStateEvent) => {
+            this.lastPoppedUrl = ev.url;
+        });
+        this.router.events.subscribe((ev) => {
+            if (ev instanceof NavigationEnd) {
+                if (ev.url === this.lastPoppedUrl) {
+                    this.lastPoppedUrl = undefined;
+                } else {
+                    window.scrollTo(0, 0);
+                }
+            }
+        });
+    }
 }
