a484dde7f206fb3db9be2279d5c748be822252c1
Dolan <dolan_miu@hotmail.com>
2017-06-20 23:05:42 +0100

    Redirects to /reports when cannot find a report

--- src/app/reports/report/report.component.ts
+++ src/app/reports/report/report.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 import { BloggerService } from '../blogger.service';
@@ -12,12 +12,15 @@ import { BloggerService } from '../blogger.service';
 export class ReportComponent implements OnInit {
     public post: Observable<Post>;
 
-    constructor(private activatedRoute: ActivatedRoute, private bloggerService: BloggerService) {
+    constructor(private activatedRoute: ActivatedRoute, private bloggerService: BloggerService, private router: Router) {
     }
 
     public ngOnInit(): void {
         this.post = this.activatedRoute.queryParams.flatMap((params) => {
             return this.bloggerService.post(params.id);
+        }).catch((err) => {
+            this.router.navigate(['/reports']);
+            return err;
         });
     }
 }
