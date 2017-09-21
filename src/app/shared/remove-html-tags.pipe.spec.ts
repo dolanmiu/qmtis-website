3fcc307879be4675b1d3df357c3e739f7a13e5f5
Dolan <dolan_miu@hotmail.com>
2017-09-21 01:02:40 +0100

    Remove spec files

--- src/app/shared/remove-html-tags.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RemoveHtmlTagsPipe } from './remove-html-tags.pipe';
-
-describe('RemoveHtmlTagsPipe', () => {
-    it('create an instance', () => {
-        const pipe = new RemoveHtmlTagsPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
