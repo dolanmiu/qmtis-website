9a3d24c83ab54c4b8969e2d95788213d2b1b7b78
Dolan <dolan_miu@hotmail.com>
2018-09-25 22:39:09 +0100

    Add first to rxjs imports

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,6 +60,7 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/first';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
