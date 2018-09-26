bd728d274b5ddcc5edc0b75ade8f462b186c400d
Dolan <dolan_miu@hotmail.com>
2018-09-26 02:30:58 +0100

    Add page scroll to learn more button

--- src/app/home/hero/hero.module.ts
+++ src/app/home/hero/hero.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
 
 import { HeroComponent } from './hero.component';
 import { MouseIconComponent } from './mouse-icon/mouse-icon.component';
 
 @NgModule({
     declarations: [HeroComponent, MouseIconComponent],
-    imports: [],
+    imports: [Ng2PageScrollModule],
     providers: [],
     bootstrap: [],
     exports: [HeroComponent],
